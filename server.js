f621996c19277200444cef76a5945b6433a0c420
Jhonathan Andres Mauricio La torre <43911632+Rigel-Developer@users.noreply.github.com>
2021-07-22 15:21:34 +0000

    firs commit

--- server.js
+++ server.js
@@ -5,6 +5,14 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+ var accepts = require('accepts');
+  //load accepts module to parse accept-language string req http header
+  //.languages methods returns an array in the order of clients preferences
+  
+  var uaParser = require('ua-parser');
+  //load ua-parser module that uses regex library to parse user agent strings in req http header
+  //Added app.enable('trust proxy') to server.js app intialization code to maket this work
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,6 +33,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
+
+    
+  app.get('/api/whoami', function(req,res){
+             
+    var ip = req.ip;
+    //get ip address with express build-in method req.ip
+    
+    var language = accepts(req).languages()[0];
+    //get an array of preferred languages from http req headers, and take the first one, which is most preferred language
+        
+    var uaHeader = req.headers['user-agent'];
+    //get user-agent string 
+    
+    var agent = uaParser.parseOS(uaHeader).toString();
+    //parse OS part of user-agent string to a string
+     
+     res.json({"ipaddress": ip, "language": language, "software": agent});
+      
+  });
 
 
 // listen for requests :)
